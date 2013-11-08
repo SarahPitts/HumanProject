1ff9b1276ad3d77f3689d455e7e876f6821fd519
Sarah Pitts <mccallsmedia@gmail.com>
2013-11-08 11:58:24 -0600

    adds gender and clothing

--- human_project.rb
+++ human_project.rb
@@ -10,14 +10,31 @@ class HumanProject
     puts "Your human's name is #@name."
   end
 
+  def gender
+    print "What is your human's gender?"
+    @gender = gets.chomp()
+
+    puts "#@name's gender is #@gender."
+  end
+
   def attributes
     print "What is your human's eye color? "
     @eyes = gets.chomp()
 
+    print "What is the color of your human's hair? "
+    @hair = gets.chomp()
+
     print "What is your human's complection? "
     @complection = gets.chomp()
 
-    puts "#@name has a #@complection and #@eyes eyes."
+    puts "#@name is a #@gender with a #@complection complection paired with #@hair hair and #@eyes eyes."
+  end
+
+  def clothing
+    print "What is your human's clothing? "
+    @clothing = gets.chomp()
+
+    puts "#@name's is wearing #@clothing."
   end
 
   def personality
@@ -32,12 +49,14 @@ class HumanProject
     @activity = gets.chomp()
 
     puts "#@name is #@activity."
-    puts "#@name is #@mood to be #@activity in #@wears."
+    puts "#@name is #@mood to be #@activity in #@clothing."
   end
 end
 
 human = HumanProject.new
 human.name
+human.gender
 human.attributes
+human.clothing
 human.personality
 human.activity
